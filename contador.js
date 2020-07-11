d5e64c12d3a138bfad0034bdf624b796a9763899
Dhayana <contatodhayana@gmail.com>
2020-07-11 15:56:12 -0300

    atualizacao de valor  da url na tela

--- contador.js
+++ contador.js
@@ -1,14 +1,20 @@
-const dataFutura = moment("2020-07-11")
+const elementoDias = document.querySelector('#dias span')
+const elementoHoras = document.querySelector('#horas span')
+const elementoMinutos = document.querySelector('#minutos span')
+const elementoSegundos = document.querySelector('#segundos span')
+
+const dataURL   = window.location.search.replace("?", "").split("=")[1]
+const dataFutura = moment(dataURL.split('/').reverse().join('-'))
 const dataAtual = moment()
 
 
 function contador() {
-
-    let verificaValidadeData = moment(dataFutura).isSameOrBefore(dataAtual)
+    let verificaValidadeData = moment(dataFutura).isSameOrBefore(dataAtual) || !dataFutura.isValid()
     
     if(verificaValidadeData) {
         atualizaValores([])
-        return;
+        alert("Data inserida é inválida, atual ou já passou!")
+        return
     }
 
     let diferencaDatasEmSegundos = dataFutura.diff(dataAtual, 'seconds')
@@ -20,7 +26,10 @@ function contador() {
 }
 
 function atualizaValores([dias = 0, horas = 0, minutos = 0, segundos = 0]) {
-    console.log(dias, horas, minutos, segundos)
+    elementoDias.textContent = dias
+    elementoHoras.textContent = horas
+    elementoMinutos.textContent = minutos
+    elementoSegundos.textContent = segundos
 }
 
 contador()
